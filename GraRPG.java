bf5c460713dc0f306c91a6038113d75ac0feadf3
BongoMajster <154436065+BongoMajster@users.noreply.github.com>
2025-10-24 13:12:01 +0200

    GraRPG v2
    Wersja 2 (Najprawdopodobniej ostatnia)

--- GraRPG.java
+++ GraRPG.java
@@ -1,5 +1,55 @@
 import java.util.Scanner;
-public class ProstaGraRPG {
+
+
+interface Attackable {
+    void attack(Attackable target);
+    String getName();
+    int getHealth();
+    void takeDamage(int damage);
+}
+
+
+class Character implements Attackable {
+    protected String name;
+    protected int level;
+    protected int health;
+
+    public Character(String name, int level, int health) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+    }
+
+    @Override
+    public void attack(Attackable target) {
+        int damage = this.level * 2;
+        System.out.println(this.name + " atakuje " + target.getName() + " i zadaje " + damage + " obrażeń.");
+        target.takeDamage(damage);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        this.health -= damage;
+        if (this.health < 0) this.health = 0;
+        System.out.println(this.name + " ma teraz " + this.health + " punktów życia.");
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+}
+
+public class GraRPG {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("=== KREATOR POSTACI RPG ===");
@@ -8,7 +58,7 @@ public class ProstaGraRPG {
         System.out.println("Wybierz rase:");
         System.out.println("1. Elf");
         System.out.println("2. Ork");
-        System.out.println("3. Czlowiek");
+        System.out.println("3. Człowiek");
         System.out.println("4. Wampir");
         System.out.println("5. Ghoul");
         System.out.print("Twój wybór: ");
@@ -33,6 +83,11 @@ public class ProstaGraRPG {
         String kolorWlosow = scanner.nextLine();
         System.out.print("Podaj kolor oczu: ");
         String kolorOczu = scanner.nextLine();
+
+
+        Character postac1 = new Character(imie, 5, 100);
+        Character postac2 = new Character("Przeciwnik", 3, 80);
+
         System.out.println("\n=== TWOJA POSTAC ===");
         System.out.println("Imie: " + imie);
         System.out.println("Rasa: " + rasa);
@@ -42,5 +97,22 @@ public class ProstaGraRPG {
         System.out.println("Kolor wlosow: " + kolorWlosow);
         System.out.println("Kolor oczu: " + kolorOczu);
         System.out.println("====================");
+
+
+        System.out.println("\nRozpoczynamy walkę!");
+        while (postac1.isAlive() && postac2.isAlive()) {
+            postac1.attack(postac2);
+            if (!postac2.isAlive()) {
+                System.out.println(postac2.getName() + " został pokonany!");
+                break;
+            }
+            postac2.attack(postac1);
+            if (!postac1.isAlive()) {
+                System.out.println(postac1.name + " został pokonany!");
+                break;
+            }
+        }
+
+        System.out.println("Koniec walki!");
     }
 }
